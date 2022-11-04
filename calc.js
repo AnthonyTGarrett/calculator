38939ae0f89c69b0d4085d58b1d3368b81fed64a
Anthony Garrett <tony.garrett@gmail.com>
2022-11-03 20:43:59 -0400

    Getting a little closer

--- calc.js
+++ calc.js
@@ -1,20 +1,57 @@
+let operandArr = [];
 let displayValue = "";
 
-const buttons = Array.from(document.querySelectorAll(".operand"));
+const resetButton = document.querySelector(".allClear");
+const operands = Array.from(document.querySelectorAll(".operand"));
+const operators = Array.from(document.querySelectorAll(".operator"));
 const display = document.querySelector(".display");
 
-buttons.forEach((element) => {
-  element.addEventListener("click", (e) => {
-    if (displayValue == "") {
-      display.textContent = displayValue;
+const evaluate = (operator, currentDisplay) => {
+  console.log("We made it");
+  if (operandArr.length === 0) {
+    operandArr.push(parseInt(currentDisplay));
+    console.log(operandArr);
+  } else if (operandArr.length === 1) {
+    displayValue = parseInt(
+      operate(operator, operandArr.pop(), parseInt(currentDisplay))
+    );
+    display.textContent = displayValue;
+    displayValue = "";
+  }
+};
+
+const updateDisplay = (currentValue) => {
+  displayValue = currentValue;
+  display.textContent = displayValue;
+};
+
+resetButton.addEventListener("click", () => {
+  displayValue = "";
+  display.textContent = 0;
+  operandArr = [];
+});
+
+operands.forEach((element) => {
+  element.addEventListener("click", (event) => {
+    if (operandArr.length === 1) {
+      updateDisplay("");
+      displayValue = displayValue + event.target.textContent;
+      updateDisplay(displayValue);
+    } else {
+      displayValue = displayValue + event.target.textContent;
+      updateDisplay(displayValue);
     }
-    displayValue = e.target.textContent;
-    display.textContent = display.textContent + displayValue;
   });
 });
 
-console.log(buttons);
+operators.forEach((element) => {
+  element.addEventListener("click", (event) => {
+    evaluate(event.target.textContent, displayValue);
+  });
+});
 
+// Math functions
+// Returns the result of the math problem
 const add = (num1, num2) => {
   return num1 + num2;
 };
@@ -28,9 +65,15 @@ const multiply = (num1, num2) => {
 };
 
 const divide = (num1, num2) => {
+  if (num2 === 0) {
+    updateDisplay("Don't even try it!");
+    return;
+  }
   return num1 / num2;
 };
 
+// Determine which math function to call
+// Returns result of math operation
 const operate = (operator, num1, num2) => {
   switch (operator) {
     case "+":
